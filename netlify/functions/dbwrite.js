e042a1b964bb4a1299d4c39cdad39f59ff82e5a3
Henry James <henryjaames@gmail.com>
2023-04-13 17:02:14 +0100

    Swapped out API key with process.env variable

--- netlify/functions/dbwrite.js
+++ netlify/functions/dbwrite.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDDbYbPXP-yycOxJOhvYmulSLGtTD3CNyk",
+  apiKey: process.env.DB_API_KEY,
   authDomain: "rememberme-database.firebaseapp.com",
   projectId: "rememberme-database",
   storageBucket: "rememberme-database.appspot.com",
