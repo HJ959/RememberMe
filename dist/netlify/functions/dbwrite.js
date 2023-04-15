2084c5ad0f31880a6a608a81593f77e671428d29
Henry James <henryjaames@gmail.com>
2023-04-15 18:43:27 +0100

    addded package.json to the netlify/function folder for firebase-admin

--- dist/netlify/functions/dbwrite.js
+++ dist/netlify/functions/dbwrite.js
@@ -7,6 +7,25 @@ admin.initializeApp({
   databaseURL: 'https://rememberme-database.firebaseapp.com'
 })
 
+// Set up an instance of the DB
+const db = admin.firestore()
+
+// exports.handler is required by netlify to process.
+exports.handler = async (event, context, callback) => {
+  // wait for the record to be added
+  await db.collection('COLLECTION').add({
+    name: 'Test'
+  })
+
+  // Return a callback witha 200 response and a message.
+  return callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: `Test data added successfully`
+    })
+  })
+}
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // const firebaseConfig = {
