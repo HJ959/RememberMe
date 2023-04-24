91f85057c6aa69957aba6f5e7ae85941d8c7ee00
Henry James <henryjaames@gmail.com>
2023-04-24 15:26:34 +0100

    dbwrite.js

--- dist/netlify/functions/dbwrite.js
+++ dist/netlify/functions/dbwrite.js
@@ -14,17 +14,19 @@ import {
 //   databaseURL: 'https://rememberme-database.firebaseapp.com'
 // })
 
+const firebaseConfig = {
+  apiKey: process.env.DB_API_KEY,
+  authDomain: "rememberme-database.firebaseapp.com",
+  projectId: "rememberme-database",
+  storageBucket: "rememberme-database.appspot.com",
+  messagingSenderId: "773885761161",
+  appId: "1:773885761161:web:592197f93d6b8a9c179858",
+  measurementId: "G-C04M7527LF"
+};
+
 exports.handler = async event => {
   const payload = JSON.parse(event.body)
-  return uploadString(ref(getStorage(initializeApp({
-    apiKey: process.env.DB_API_KEY,
-    authDomain: "rememberme-database.firebaseapp.com",
-    projectId: "rememberme-database",
-    storageBucket: "rememberme-database.appspot.com",
-    messagingSenderId: "773885761161",
-    appId: "1:773885761161:web:592197f93d6b8a9c179858",
-    measurementId: "G-C04M7527LF"
-  })), payload.name), payload.file.split(',')[1], 'base64').then(file => {
+  return uploadString(ref(getStorage(initializeApp(firebaseConfig)), payload.name), payload.file.split(',')[1], 'base64').then(file => {
     return getDownloadURL(file.ref).then(url => {
       return {
         body: JSON.stringify({
