4f496d342944fdf7b8f42bf6cfd22b48123c929e
Henry James <henryjaames@gmail.com>
2023-05-01 15:45:42 +0100

    buffer

--- dist/netlify/functions/dbwrite.js
+++ dist/netlify/functions/dbwrite.js
@@ -7,7 +7,7 @@ import {
   ref
 } from "firebase/storage";
 
-import * as busboy from "busboy"
+const Buffer = require('buffer/').Buffer
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
@@ -31,56 +31,20 @@ const storageRef = ref(storage, 'audio');
 
 exports.handler = async event => {
   console.log(event)
-
-  const fields = await parseMultipartForm(event)
-
-  console.log(fields)
-
-  uploadBytes(storageRef, fields).then((snapshot) => {
+  let base64toString = Buffer.from(event.body, "base64").toString()
+  console.log(base64toString)
+  
+  uploadBytes(storageRef, base64toString).then((snapshot) => {
     console.log('Uploaded audio file!')
   });
 
-  return 200;
-}
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "GET, POST, OPTION",
+    },
+  }
 
-function parseMultipartForm(event) {
-  return new Promise((resolve) => {
-    // we'll store all form fields inside of this
-    const fields = {};
-
-    // let's instantiate our busboy instance!
-    console.log(busboy)
-    const busboy = busboy({headers: event.headers});
-
-    // before parsing anything, we need to set up some handlers.
-    // whenever busboy comes across a file ...
-    busboy.on(
-      "file",
-      (fieldname, filestream, filename, transferEncoding, mimeType) => {
-        // ... we take a look at the file's data ...
-        filestream.on("data", (data) => {
-          // ... and write the file's name, type and content into `fields`.
-          fields[fieldname] = {
-            filename,
-            type: mimeType,
-            content: data,
-          };
-        });
-      }
-    );
-
-    // whenever busboy comes across a normal field ...
-    busboy.on("field", (fieldName, value) => {
-      // ... we write its value into `fields`.
-      fields[fieldName] = value;
-    });
-
-    // once busboy is finished, we resolve the promise with the resulted fields.
-    busboy.on("finish", () => {
-      resolve(fields)
-    });
-
-    // now that all handlers are set up, we can finally start processing our request!
-    busboy.write(event.body);
-  });
 }
