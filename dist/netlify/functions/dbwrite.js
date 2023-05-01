c99ef28d609ae1664cfdaddf2f9f723fccc933fe
Henry James <henryjaames@gmail.com>
2023-05-01 14:05:28 +0100

    busboy

--- dist/netlify/functions/dbwrite.js
+++ dist/netlify/functions/dbwrite.js
@@ -1,5 +1,13 @@
-import { initializeApp } from "firebase/app";
-import { getStorage, uploadBytes, ref } from "firebase/storage";
+import {
+  initializeApp
+} from "firebase/app";
+import {
+  getStorage,
+  uploadBytes,
+  ref
+} from "firebase/storage";
+
+import * as Busboy from "busboy"
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
@@ -31,3 +39,47 @@ exports.handler = async event => {
   return 200;
 }
 
+function parseMultipartForm(event) {
+  return new Promise((resolve) => {
+    // we'll store all form fields inside of this
+    const fields = {};
+
+    // let's instantiate our busboy instance!
+    const busboy = new Busboy({
+      // it uses request headers
+      // to extract the form boundary value (the ----WebKitFormBoundary thing)
+      headers: event.headers
+    });
+
+    // before parsing anything, we need to set up some handlers.
+    // whenever busboy comes across a file ...
+    busboy.on(
+      "file",
+      (fieldname, filestream, filename, transferEncoding, mimeType) => {
+        // ... we take a look at the file's data ...
+        filestream.on("data", (data) => {
+          // ... and write the file's name, type and content into `fields`.
+          fields[fieldname] = {
+            filename,
+            type: mimeType,
+            content: data,
+          };
+        });
+      }
+    );
+
+    // whenever busboy comes across a normal field ...
+    busboy.on("field", (fieldName, value) => {
+      // ... we write its value into `fields`.
+      fields[fieldName] = value;
+    });
+
+    // once busboy is finished, we resolve the promise with the resulted fields.
+    busboy.on("finish", () => {
+      resolve(fields)
+    });
+
+    // now that all handlers are set up, we can finally start processing our request!
+    busboy.write(event.body);
+  });
+}
