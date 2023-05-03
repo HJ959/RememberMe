bd8ec0273b5ecfc32e11bf9e8c9a1a5bcf06ae7f
Henry James <henryjaames@gmail.com>
2023-05-03 09:44:35 +0100

    sorted dbwrite some more to upload bytes

--- dist/netlify/functions/dbwrite.js
+++ dist/netlify/functions/dbwrite.js
@@ -30,12 +30,11 @@ const storage = firebaseStorage.getStorage(app);
 exports.handler = async event => {
   console.log(event)
   //let audioFile = Buffer.from(event.body, "base64")
-  console.log("Attempting to create a Blob from the base64 string")
+  // console.log("Attempting to create a Blob from the base64 string")
   // let audioFile = firebaseFirestore.Blob.fromBase64String(event.body)
 
-  console.log(audioFile)
-
   const storageRef = firebaseStorage.ref(storage, "audio");
+  console.log(storageRef)
 
   let metadata = {
     contentType: 'audio/m4a'
