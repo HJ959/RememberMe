13c3974c20575e94281cfc7b76e09480b6246c50
Henry James <henryjaames@gmail.com>
2023-05-03 08:51:00 +0100

    sorted dbwrite some more to upload bytes

--- dist/netlify/functions/dbwrite.js
+++ dist/netlify/functions/dbwrite.js
@@ -1,5 +1,11 @@
-import * as firebase from "firebase/"
-// const Buffer = require('buffer').Buffer
+import {
+  initializeApp
+} from "firebase/app";
+
+import * as firebaseStorage from "firebase/storage"
+import * as firebaseFirestore from 'firebase/firestore'
+
+const Buffer = require('buffer').Buffer
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
@@ -14,29 +20,29 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 // Initialize Cloud Storage and get a reference to the service
 // const storage = firebase.getStorage(app);
-const storage = firebase.storage.getStorage(app);
+const storage = firebaseStorage.getStorage(app);
 
 
 exports.handler = async event => {
   console.log(event)
   //let audioFile = Buffer.from(event.body, "base64")
-  let audioFile = firebase.firestore.Blob.fromBase64String(event.body)
+  let audioFile = firebaseFirestore.Blob.fromBase64String(event.body)
   console.log(audioFile)
   console.log(event.body)
 
-  const storageRef = firebase.storage.ref(storage, "audio");
-
+  const storageRef = firebaseStorage.ref(storage, "audio");
+  
   let metadata = {
     contentType: 'audio/m4a'
   }
 
   // var blob = new Blob([audioFile], {type: 'audio/mp3'});
   // storageRef.put(blob);
-  firebase.storage.uploadBytes(storageRef, audioFile, metadata).then((snapshot) => {
+  firebaseStorage.uploadBytes(storageRef, audioFile, metadata).then((snapshot) => {
     console.log('Uploaded audio file!')
   });
 
