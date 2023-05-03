da4fd88c46c8fc12e6e2053a70296d3f4056111c
Henry James <henryjaames@gmail.com>
2023-05-03 10:11:51 +0100

    some stuff

--- netlify/functions/dbwrite.js
+++ netlify/functions/dbwrite.js
@@ -46,6 +46,7 @@ exports.handler = async event => {
   //   console.log('Uploaded audio file!')
   // });
   // Data URL string
+  firebaseStorage.uploadString(storageRef, event.body, 'data_url')
   firebaseStorage.uploadString(storageRef, event.body, 'data_url').then((snapshot) => {
     console.log('Uploaded a data_url string!');
     console.log(snapshot);
