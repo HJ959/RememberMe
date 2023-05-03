68a898ee86828b87b7ceed27b332d34dc19ed24a
Henry James <henryjaames@gmail.com>
2023-05-03 08:48:06 +0100

    sorted dbwrite some more to upload bytes

--- netlify/functions/dbwrite.js
+++ netlify/functions/dbwrite.js
@@ -1,4 +1,4 @@
-import * as firebase from "firebase"
+import * as firebase from "firebase/"
 // const Buffer = require('buffer').Buffer
 
 // TODO: Replace the following with your app's Firebase project configuration
